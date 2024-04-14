a711d3457946b6421d20a5fd253cd47fff654253
cardgis <161532830+cardgis@users.noreply.github.com>
2024-04-14 22:47:14 +0200

    Mise en place du header

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
+"use client";
 import Image from "next/image";
+import Countdown from "@/components/Countdown";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-        <p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Lets go damn;
-          <code className="font-mono font-bold">src/app/page.js</code>
+        <p className="flex justify-start w-full gap-3 pt-8 pb-6">
+          <code className="font-mono text-lg font-bold">Home</code>
+          <code className="font-mono text-lg font-bold">About-Us </code>
+          <code className="font-mono text-lg font-bold">Contact</code>
         </p>
         <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -15,7 +18,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            Logo a venir{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -27,7 +30,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -37,6 +39,9 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+      <div>
+        <Countdown></Countdown>
       </div>
 
       <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
