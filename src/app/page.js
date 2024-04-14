fd82a58e3c27aff3960ce26649d089367bcb0575
cardgis <161532830+cardgis@users.noreply.github.com>
2024-04-14 23:55:29 +0200

    update countdown

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,22 @@
 "use client";
 import Image from "next/image";
-import Countdown from "@/components/Countdown";
-
+import Countdown from "@/components/countdown";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         <p className="flex justify-start w-full gap-3 pt-8 pb-6">
-          <code className="font-mono text-lg font-bold">Home</code>
-          <code className="font-mono text-lg font-bold">About-Us </code>
-          <code className="font-mono text-lg font-bold">Contact</code>
+          <code className="font-mono text-lg font-bold rounded-lg cursor-pointer hover:bg-amber-600 hover:text-white">
+            Home
+          </code>
+          <code className="font-mono text-lg font-bold rounded-lg cursor-pointer hover:bg-amber-600 hover:text-white">
+            About-Us{" "}
+          </code>
+          <code className="font-mono text-lg font-bold rounded-lg cursor-pointer hover:bg-amber-600 hover:text-white">
+            Contact
+          </code>
         </p>
-        <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 cursor-pointer bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -30,7 +35,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -39,9 +44,12 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
-      <div>
-        <Countdown></Countdown>
+      </div> */}
+
+      <span className="">Premier départ le 05 septembre 2024</span>
+
+      <div className="bg-orange-300">
+        <Countdown />
       </div>
 
       <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
